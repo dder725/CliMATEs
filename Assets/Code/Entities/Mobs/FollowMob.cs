7e153a02600cc73f916a09150594bdf160a0b287
Magnus Rand <magnus.rand@gmail.com>
2019-09-30 11:24:46 +1300

    Initial Commit
    Including basic world generator, player movement and following NPC as a starting point. Nothing here need to be kept. It's ment as a starting point.

---
+++ Assets/Code/Entities/Mobs/FollowMob.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMob : Entity
+{
+    public Entity following;
+    public float distance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (following.GetComponent<Rigidbody2D>().transform.position.y > (GetComponent<Rigidbody2D>().transform.position.y + distance)) {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (following.GetComponent<Rigidbody2D>().transform.position.y < (GetComponent<Rigidbody2D>().transform.position.y - distance))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (following.GetComponent<Rigidbody2D>().transform.position.x > (GetComponent<Rigidbody2D>().transform.position.x + distance))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (following.GetComponent<Rigidbody2D>().transform.position.x < (GetComponent<Rigidbody2D>().transform.position.x - distance))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Code/Entities/Player/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+    }
+}
