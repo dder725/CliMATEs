38f76e7598e44c0c650736ffc91f6d804d03b171
Magnus Rand <magnus.rand@gmail.com>
2019-10-04 12:56:25 +1300

    Add flower placing functionality #1

---
+++ Assets/Code/Entities/Player/BlockPlacing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPlacing : MonoBehaviour
+{
+    public Transform block;
+    public int numberOfBlocks;
+    public Sprite[] sprites;
+
+    public Entity player;
+
+    private System.Random random = new System.Random();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && numberOfBlocks > 0)
+        {
+            block.GetComponent<SpriteRenderer>().sprite = sprites[random.Next(0, sprites.Length)];
+            Instantiate(block, new Vector3(player.transform.position.x, player.transform.position.y), Quaternion.identity);
+            numberOfBlocks--;
+        }
+    }
+}
