aea0658067a29612a5f0d267e5dac1d59de8aa02
Lola Garden <lola.c.garden@gmail.com>
2019-10-06 16:32:52 +1300

    Updating the HUD
    Fixing minimap, adding animal tokens

---
+++ Assets/AnimalTokensScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalTokensScript : MonoBehaviour
+{
+
+    public Button beeButton;
+    public Button butterflyButton;
+    public Button turtleButton;
+    public Button polarBearButton;
+    [SerializeField] private Text noAnimalsText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        beeButton.gameObject.SetActive(false);
+        butterflyButton.gameObject.SetActive(false);
+        turtleButton.gameObject.SetActive(false);
+        polarBearButton.gameObject.SetActive(false);
+
+        noAnimalsText.gameObject.SetActive(true);
+
+
+
+    }
+
+
+    public void ShowBeeToken()
+    {
+        //Disable the 'no animals' text
+        noAnimalsText.gameObject.SetActive(false);
+        beeButton.enabled = true;
+    }
+
+    public void ShowButterflyToken()
+    {
+        butterflyButton.enabled = true;
+    }
+
+    public void ShowTurtleToken()
+    {
+        turtleButton.enabled = true;
+    }
+
+    public void ShowPolarBearToken()
+    {
+        polarBearButton.enabled = true;
+    }
+
+
+
+
+
+}
