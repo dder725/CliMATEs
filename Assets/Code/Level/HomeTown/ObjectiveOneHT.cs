6dc71b962a9f85d6adf5de916c402d108693e9dd
Magnus Rand <magnus.rand@gmail.com>
2019-10-06 17:45:57 +1300

    Fix merge error with working scene #5

---
+++ Assets/Code/Level/HomeTown/ObjectiveOneHT.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveOneHT : Objective
+{
+    public Transform player;
+
+    public override void GiveObjectiveRewards()
+    {
+  
+    }
+
+    public override bool ObjectiveGoalIsAchieved()
+    {
+        return player.transform.position.x < 10;
+    }
+
+    public override void RunStartUpLogicForObjective()
+    {
+
+    }
+
+    public override void RunTearDownLogicForObjective()
+    {
+
+    }
+}
