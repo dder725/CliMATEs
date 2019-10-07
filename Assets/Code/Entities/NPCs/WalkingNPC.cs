76fc81ad10d00f650e5974a160cdd77f4c24ac95
Rupert Moran <rupert.moran.adcock@gmail.com>
2019-10-07 17:28:50 +1300

    Adding more objectives - butterflies etc

---
+++ Assets/Code/Entities/NPCs/WalkingNPC.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingNPC : Entity
+{
+
+    private Rigidbody2D myRigidbody2D;
+    public bool isWalking;
+    public float walkTime;
+    //0 = up, 1 = right, 2 = down, 3 = left
+    public int direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isWalking)
+        {
+            walkTime -= Time.deltaTime;
+            switch (direction)
+            {
+                case 0:
+                    myRigidbody2D.velocity = new Vector2(0, speed);
+                    break;
+
+                case 1:
+                    myRigidbody2D.velocity = new Vector2(0, speed);
+                    break;
+
+                case 2:
+                    myRigidbody2D.velocity = new Vector2(0, speed);
+                    break;
+
+                case 3:
+                    myRigidbody2D.velocity = new Vector2(0, speed);
+                    break;
+            }
+
+            if(walkTime < 0)
+            {
+                isWalking = false;
+            }
+        }
+
+    }
+}
