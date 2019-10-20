0668b9978987bf8eb0cf1f11439773cbd6f2fb88
Tia Saunders <tsau354@aucklanduni.ac.nz>
2019-10-21 10:25:02 +1300

    Next Level screen

---
+++ Assets/Code/Level/HomeTown/NexLevelScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NexLevelScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene("BigCity_magnus");
+    }
+
+    public void QuitGame() 
+    {
+        Application.Quit();
+    }
+
+
+}
