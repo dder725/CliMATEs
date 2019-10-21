ce52b7f3f5a2225caf9aafaa58e0c5679a9a5612
Rupert Moran <rupert.moran.adcock@gmail.com>
2019-10-21 19:20:10 +1300

    Objective7 WIP

---
+++ Assets/Code/Level/BigCity/Quest2SavingDinosaurs/Objective7City.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective7City : Objective
+{
+    public override void GiveObjectiveRewards()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool ObjectiveGoalIsAchieved()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void RunStartUpLogicForObjective()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void RunTearDownLogicForObjective()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
