276d3c45e89616cba5e0d438dea0e278b2c62839
Rupert Moran <rupert.moran.adcock@gmail.com>
2019-10-21 19:36:01 +1300

    Added wandering NPCs to city

--- Assets/Code/Level/BigCity/Quest2SavingDinosaurs/Objective7City.cs
+++ Assets/Code/Level/BigCity/Quest2SavingDinosaurs/Objective7City.cs
@@ -4,24 +4,52 @@ using UnityEngine;
 
 public class Objective7City : Objective
 {
+
+    public Transform factoryWorker;
+    private WanderingTalkingNPC factoryWorkerScript;
+
+    public Transform citizen1;
+    private WanderingTalkingNPC npcScript1;
+
+    public Transform citizen2;
+    private WanderingTalkingNPC npcScript2;
+
+    public Transform citizen3;
+    private WanderingTalkingNPC npcScript3;
+
+    public Transform citizen4;
+    private WanderingTalkingNPC npcScript4;
+
+    public Transform citizen5;
+    private WanderingTalkingNPC npcScript5;
     public override void GiveObjectiveRewards()
     {
-        throw new System.NotImplementedException();
+
     }
 
     public override bool ObjectiveGoalIsAchieved()
     {
-        throw new System.NotImplementedException();
+        return factoryWorkerScript.ConversationFinished();
     }
 
     public override void RunStartUpLogicForObjective()
     {
-        throw new System.NotImplementedException();
+        factoryWorkerScript = factoryWorker.GetComponent<WanderingTalkingNPC>();
+
+        npcScript1 = citizen1.GetComponent<WanderingTalkingNPC>();
+
+        npcScript2 = citizen2.GetComponent<WanderingTalkingNPC>();
+
+        npcScript3 = citizen3.GetComponent<WanderingTalkingNPC>();
+
+        npcScript4 = citizen4.GetComponent<WanderingTalkingNPC>();
+
+        npcScript5 = citizen5.GetComponent<WanderingTalkingNPC>();
     }
 
     public override void RunTearDownLogicForObjective()
     {
-        throw new System.NotImplementedException();
+
     }
 
 }
