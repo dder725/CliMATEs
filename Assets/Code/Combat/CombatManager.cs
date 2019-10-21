750c1e40e8a701f582eb6b0d84d245a1ee96171d
Lola Garden <lola.c.garden@gmail.com>
2019-10-21 20:23:29 +1300

    Master is working again

--- Assets/Code/Combat/CombatManager.cs
+++ Assets/Code/Combat/CombatManager.cs
@@ -57,6 +57,8 @@ public class CombatManager : MonoBehaviour
         EventManager.StartListening("combatStart", new UnityAction(startCombat));
         EventManager.StartListening("combatNextTurn", new UnityAction(nextTurn));
         EventManager.StartListening("combatExit", new UnityAction(exitCombat));
+
+
     }
 
     // Update is called once per frame
@@ -182,6 +184,14 @@ public class CombatManager : MonoBehaviour
             // Return player to the original location
             player.transform.position = playerOriginalPosition.position;
 
+            //set as defeated
+
+            Monster mobScript = mob.GetComponent<Monster>();
+            mobScript.defeated = true;
+
+            Debug.Log(mobScript.defeated);
+            
+
             // Remove mob
             Destroy(mob);
             mob = null;
@@ -213,5 +223,9 @@ public class CombatManager : MonoBehaviour
         // Switch cameras
         GameObject.Find("Combat Camera").GetComponent<Camera>().enabled = false;
         GameObject.Find("Main Camera").GetComponent<Camera>().enabled = true;
+
+        // Trigger an achievement (Will only work for the first battle)
+        AchievementManager.ach04Trigger = true;
+
     }
 }
