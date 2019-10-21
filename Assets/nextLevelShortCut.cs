a86e8f6cf680955b9cbffb8c0c1f4b6dd1376a1d
dder725 <dder725@aucklanduni.ac.nz>
2019-10-21 15:20:45 +1300

    HUD object is now transferred between the scenes

---
+++ Assets/nextLevelShortCut.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextLevelShortCut : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("COllided with a beehive");
+        SceneManager.LoadScene("NextLevelScreen");
+    }
+}
